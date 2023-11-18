7cfaff972567007529b7c5d17ddb9b22e7aa3754
Alejandro Arango <alejandroagarcia19@gmail.com>
2023-11-18 15:43:38 -0500

    Cambios Inicio GIT

--- inicio.py
+++ inicio.py
@@ -1,4 +1,5 @@
 import streamlit as st
 
-#Diseño personalizado
-st.header("Inicio")
+st.markdown("""
+<img src="https://firebasestorage.googleapis.com/v0/b/prueba-64c31.appspot.com/o/proyecto.gif?alt=media&token=889e94df-2248-4796-a089-50c11373f17f" width="100%" height="400">
+""", unsafe_allow_html=True)
